407fcc39df3b68538d90db7f5b7c87e04a89470a
@lxemily <34338522+LxEmily@users.noreply.github.com>
2019-09-20 22:34:32 +0100

    Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,9 +98,14 @@ module.exports = {
       },
     },   
     `gatsby-plugin-offline`,
-    
-    
-    
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: config.meta.googleAnalytics,
+        anonymize: true,
+        respectDNT: true,
+      },
+    }, 
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
